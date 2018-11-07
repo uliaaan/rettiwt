130099b23d5e33420a77a32fafac93d88d26e870
Yl Markov <markovopg@gmail.com>
2018-11-08 00:11:41 +0300

    heroku deploy

--- express/server.js
+++ express/server.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
@@ -5,6 +6,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
@@ -37,6 +39,11 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/posts', posts);
 
+app.use(express.static(path.join(__dirname, '../react-redux/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '../react-redux/build/index.html'));
+});
 const port = process.env.PORT || 5000;
 
 http.listen(port, () => console.log(`Server start on port ${port}`));
